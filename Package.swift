42be4d2f113ff6593281fb46e011420abc7a5ced
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-22 17:22:25 +0300

    Add xcframework

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "MpesaSDK",
+    platforms: [
+        .iOS(.v13),
+        .macCatalyst(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +22,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MpesaSDK",
-            dependencies: []),
-        .testTarget(
-            name: "MpesaSDKTests",
-            dependencies: ["MpesaSDK"]),
+        .binaryTarget(name: "MpesaSDK", path: "Sources/MpesaSDK.xcframework")
     ]
 )
---
+++ Sources/MpesaSDK.xcframework/ios-arm64_x86_64-maccatalyst/MpesaSDK.framework/Versions/A/Headers/MpesaSDK.h
@@ -0,0 +1,18 @@
+//
+//  MpesaSDK.h
+//  MpesaSDK
+//
+//  Created by Ahmed Mgua on 22/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MpesaSDK.
+FOUNDATION_EXPORT double MpesaSDKVersionNumber;
+
+//! Project version string for MpesaSDK.
+FOUNDATION_EXPORT const unsigned char MpesaSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MpesaSDK/PublicHeader.h>
+
+
